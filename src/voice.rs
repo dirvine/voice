824485646ba522f5da903f7ecd15b8a356ba6c77
David Irvine <david.irvine@maidsafe.net>
2016-10-30 23:28:28 +0000

    More structure, still needs graph analysis and intent parser

--- src/voice.rs
+++ src/voice.rs
@@ -1,12 +1,25 @@
 use std::process::Command;
 
+/// Speaks a string
 pub fn speak(string: &str) {
-    Command::new("espeak")
+    let _ = Command::new("espeak")
         .arg("-p 30")
         .arg("-s 165")
         .arg("-g 3")
         .arg("-ven-sc")
         .arg(string)
-        .output() // use spawn() to ignore output and not wait on completion
+        .output()
+        .expect("espeak must be installed!");
+}
+
+/// Speaks a string asyncronously
+pub fn speak_async(string: &str) {
+    let _ = Command::new("espeak")
+        .arg("-p 30")
+        .arg("-s 165")
+        .arg("-g 3")
+        .arg("-ven-sc")
+        .arg(string)
+        .spawn()
         .expect("espeak must be installed!");
 }
